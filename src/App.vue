4e71d5bc9f6bf071848092b78b1ff994846f0804
mugu <1175233520@qq.com>
2022-03-08 15:24:30 +0800

    引入组件库

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HomePage from './views/HomePage.vue';
+import NavFooter from './components/NavFooter.vue';
+import UserLoginVue from './views/UserLogin.vue';
+import UserLogin from './views/UserLogin.vue';
+</script>
+
+<template>
+  <HomePage msg="传参测试app-->homePage"/>
+  <NavFooter/>
+  <UserLogin/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/NavFooter.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { TabbarItem, Tabbar } from "vant";
+import { ref } from "vue";
+
+const active = ref(0);
+</script>
+
+
+<template>
+    <Tabbar v-model="active">
+        <TabbarItem icon="home-o">标签</TabbarItem>
+        <TabbarItem icon="search">标签</TabbarItem>
+        <TabbarItem icon="friends-o">标签</TabbarItem>
+        <TabbarItem icon="setting-o">标签</TabbarItem>
+    </Tabbar>
+</template>
+    
+
+<style>
+</style>
